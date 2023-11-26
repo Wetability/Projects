d6bca8a27497c76e09db7999f60b8bb934df4175
Wetability <vova.podeiko@gmail.com>
2023-11-26 15:33:48 +0200

    init

---
+++ homeworks/base/index.js
@@ -0,0 +1,24 @@
+// * Name
+let username = prompt("What is ur name?");
+
+console.log("Hello: ", username);
+
+// *Age
+let year = prompt("What year were you born?");
+
+console.log("Age: ", 2023 - year);
+
+// *Gender
+let gender = prompt("What is ur gender?");
+
+console.log("Gender: ", gender);
+
+// *Square
+let square = prompt("Tell me the length of a side of a square");
+
+console.log("Square area:", square * square);
+
+// *Convert from $ to ₴ (26.11.2023)
+let convert = prompt("Convert from $ to ₴");
+
+console.log("₴:", convert * 37.45);
